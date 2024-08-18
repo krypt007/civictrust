cb44de84a33b0fa81fac3b57734265371f79b323
krypt007 <steve@calstec.com>
2024-08-18 05:36:03 -0400

    web3 login

--- v02/src/components/Dashboard/Home.tsx
+++ v02/src/components/Dashboard/Home.tsx
@@ -1,6 +1,10 @@
 'use client'
 import { useActiveAccount, useActiveWallet, useDisconnect } from "thirdweb/react";
 import { Button } from "../ui/button";
+import Header from "@/components/Header";
+
+import Link from 'next/link';
+
 
 const Home = () => {
     const {disconnect} = useDisconnect();
@@ -9,9 +13,11 @@ const Home = () => {
   return (
     <div className="flex flex-col justify-between bg-purple-950 px-5">
 
-        {/** Header */}
-        <div className="fixed w-full top-0 left-0 z-50 transition-all flex justify-between p-2 bg-[#9003fc]">
-            <div className="flex flex-col text-white">
+       
+        <div className="fixed w-full top-0 left-0 z-50 transition-all flex justify-between p-2 bg-blue-800">
+            <div className="flex text-white p-2 gap-5 justify-end ">
+                <Link href="/" className="hover:text-gray-400">Dashboard</Link>
+                <Link href="/projects" className="hover:text-gray-400">Projects</Link>
                 
             </div>
             <Button onClick={()=> {disconnect(connectedWallet!)}}>Logout</Button>
