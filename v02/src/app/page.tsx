63a1cc1dcac29994762d661fe20cf894fd00f0ab
krypt007 <steve@calstec.com>
2024-08-18 04:30:36 -0400

    updated homepage

--- v02/src/app/page.tsx
+++ v02/src/app/page.tsx
@@ -29,23 +29,99 @@ const Home = () => {
           <div>Your Transparency Partner in Government Projects and Fiscal Planning.</div>
           <Link href={`/admin`}><Button>Get Started</Button></Link>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-purple-300">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-slate-200">
+          {/** row 1 */}
           <Card>
-            <CardHeader>New Projects</CardHeader>
+            <CardHeader className='text-xl font-bold'>New Projects</CardHeader>
+            <div className='flex w-full justify-center items-center'>
+              <div className='flex w-16 h-16 object-contain'>
+                <Image 
+                src={`/icons/add.png`}
+                alt='Nairobi skyline'
+                width={2000}
+                height={1000}
+                />
+              </div>
+            </div>
             <CardContent>A list of newly started or about to start projects.</CardContent>
           </Card>
           <Card>
-            <CardHeader>New Projects</CardHeader>
-            <CardContent>A list of newly started or about to start projects.</CardContent>
+            <CardHeader className='text-xl font-bold'>Stalled Projects</CardHeader>
+            <div className='flex w-full justify-center items-center'>
+              <div className='flex w-16 h-16 object-contain'>
+                <Image 
+                src={`/icons/error.png`}
+                alt='Nairobi skyline'
+                width={2000}
+                height={1000}
+                />
+              </div>
+            </div>
+            <CardContent>Stalled or unfinished projects, do you live in any relevant area, talk to us.</CardContent>
           </Card>
           <Card>
-            <CardHeader>New Projects</CardHeader>
-            <CardContent>A list of newly started or about to start projects.</CardContent>
+            <CardHeader className='text-xl font-bold'>Closed Projects</CardHeader>
+            <div className='flex w-full justify-center items-center'>
+              <div className='flex w-16 h-16 object-contain'>
+                <Image 
+                src={`/icons/list.png`}
+                alt='Nairobi skyline'
+                width={2000}
+                height={1000}
+                />
+              </div>
+            </div>
+            <CardContent>Completed projects and some analysis of them.</CardContent>
           </Card>
-        <AreaGraph />
+
+          {/** row 2 */}
+          <Card>
+            <CardHeader className='text-xl font-bold'>Submit Evidence</CardHeader>
+            <div className='flex w-full justify-center items-center'>
+              <div className='flex w-16 h-16 object-contain'>
+                <Image 
+                src={`/icons/find.png`}
+                alt='Nairobi skyline'
+                width={2000}
+                height={1000}
+                />
+              </div>
+            </div>
+            <CardContent>Take photos and upload, share your views of open and closed projects and earn VUMA points.</CardContent>
+          </Card>
+          <Card>
+            <CardHeader className='text-xl font-bold'>View Rewards</CardHeader>
+            <div className='flex w-full justify-center items-center'>
+              <div className='flex w-16 h-16 object-contain'>
+                <Image 
+                src={`/icons/reward.png`}
+                alt='Nairobi skyline'
+                width={2000}
+                height={1000}
+                />
+              </div>
+            </div>
+            <CardContent>With Vuma points you can earn real money for participating to make your county better.</CardContent>
+          </Card>
+          <Card>
+            <CardHeader className='text-xl font-bold'>Complaints</CardHeader>
+            <div className='flex w-full justify-center items-center'>
+              <div className='flex w-16 h-16 object-contain'>
+                <Image 
+                src={`/icons/comment.png`}
+                alt='Nairobi skyline'
+                width={2000}
+                height={1000}
+                />
+              </div>
+            </div>
+            <CardContent>Do you have a burning issue, let us know.</CardContent>
+          </Card>
+          {/** row 3 */}
+        {/* <AreaGraph />
         <PieGraph />
         <BarGraph />
-        <ProgressGraph />
+        <ProgressGraph /> */}
         </div>
         
       </div>
